f71e6b556104d0cffbc7ee1515713e6a319e97f7
Sasha <201165090@cegepsherbrooke.qc.ca>
2025-02-11 08:22:24 -0500

    Create Exercices.py

---
+++ Exercices.py
@@ -0,0 +1,31 @@
+#1
+'''
+while True:
+    réponse = str(input("Avez-vous une technique en ce moment? "))
+    if réponse == ("Oui" or "oui" or "O"):
+        break
+print("Nice!")
+'''
+#2
+'''
+liste = ["pomme", "tarte", "viande"]
+
+for i in liste:
+    if i == "tarte":
+        print("tarte est dans la liste")
+        break
+else:
+    print("tarte n'est pas dans la liste")
+'''
+#Continuer de travailler sur votre code
+#Possible de changer
+#Deux for, deux while
+#trois input de l'utilisateur
+#des prints, genre beaucoup
+#trois bloc de if, nombre de "if"
+
+#break
+#continue
+#else (boucle)
+
+#si vous arivez au lab avec c'est quoi un "pass" vous gagnez un morceau de robot
